72a1c70a67f2cd8532420118bd527b1f8b4b6fb2
Javieruw <javier.uw@gmail.com>
2024-05-09 02:20:06 +0200

    fr

--- Http/Program.cs
+++ Http/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading;
 
-Console.WriteLine("Hello, World!");
+namespace Http
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set up HTTP listener
+            var listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Start();
+            Console.WriteLine("Listening for incoming requests...");
+
+            while (true)
+            {
+                // Wait for a request
+                var context = listener.GetContext();
+                var request = context.Request;
+
+                // Read the incoming request data
+                var requestData = new StringBuilder();
+                using (var reader = new System.IO.StreamReader(request.InputStream, request.ContentEncoding))
+                {
+                    requestData.Append(reader.ReadToEnd());
+                }
+
+                // Send back the received data as response
+                var responseData = Encoding.UTF8.GetBytes(requestData.ToString());
+                context.Response.ContentType = "text/plain";
+                context.Response.ContentEncoding = Encoding.UTF8;
+                context.Response.OutputStream.Write(responseData, 0, responseData.Length);
+                context.Response.Close();
+
+                // Output received request data
+                Console.WriteLine($"Received message: {requestData}");
+            }
+        }
+    }
+}
